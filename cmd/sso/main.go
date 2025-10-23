6b29a0e6bb3baf3a5105853a00a2717b6044d061
gitavk <3apa3ka3@gmail.com>
2025-10-23 13:33:59 +0200

    Logger.

--- cmd/sso/main.go
+++ cmd/sso/main.go
@@ -1,15 +1,44 @@
 package main
 
 import (
-	"fmt"
+	"log/slog"
+	"os"
 
 	"github.com/gitavk/sso.v1/internal/config"
 )
 
+const (
+	envLocal = "local"
+	envDev   = "dev"
+	envProd  = "prod"
+)
+
 func main() {
 	cfg := config.MustLoad()
-	fmt.Printf("Loaded config: %+v\n", cfg)
-	// TODO: logger
+	log := NewLogger(cfg.Env)
+	log.Debug("Loaded config", slog.Any("config", cfg))
+
 	// TODO: init app
 	// TODO: run gRPC server
+}
+
+func NewLogger(env string) *slog.Logger {
+	var log *slog.Logger
+
+	switch env {
+	case envLocal:
+		log = slog.New(
+			slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}),
+		)
+	case envDev:
+		log = slog.New(
+			slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}),
+		)
+	case envProd:
+		log = slog.New(
+			slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelInfo}),
+		)
+	}
+
+	return log
 }
